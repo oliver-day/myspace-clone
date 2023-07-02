b4871a062a280b257426cd5b56c1be92e32120cc
Oliver Day <oliver.day.swe@gmail.com>
2023-07-02 13:18:35 -0700

    feat: add logic to redirect user if signed out

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 import styles from './page.module.css'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
     <main className={styles.main}>
     </main>
