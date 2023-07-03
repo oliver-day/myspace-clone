dccd44a444076b22fbe6438ac26510d206be3ba4
Oliver Day <oliver.day.swe@gmail.com>
2023-07-03 15:16:54 -0700

    chore: improve log structure for 'AuthCheck'

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -4,8 +4,7 @@ import { useSession } from 'next-auth/react';
 
 export default function AuthCheck({ children }: { children: React.ReactNode}) {
     const { data: session, status } = useSession();
-
-    console.log(session, status);
+    console.log('%c[AuthCheck]', 'color: #00ff00', 'session:', session, 'status:', status);
 
     if (status === 'authenticated') {
       return <>{children}</>;
