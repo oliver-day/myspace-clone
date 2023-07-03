a7b7220eafba1def82299301538f72321df90b08
Oliver Day <oliver.day.swe@gmail.com>
2023-07-03 15:35:53 -0700

    chore: add prisma adapter to next-auth authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,19 @@
 import NextAuth from 'next-auth/next';
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/lib/prsima';
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
+
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
