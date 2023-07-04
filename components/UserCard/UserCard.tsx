ebce5f3811d5645bdb9f21dafa5efda6b5ec70aa
Oliver Day <oliver.day.swe@gmail.com>
2023-07-03 17:54:29 -0700

    feat: add null check for age in 'UserCard'

--- components/UserCard/UserCard.tsx
+++ components/UserCard/UserCard.tsx
@@ -20,7 +20,7 @@ export default function UserCard({ id, name, age, image }: Props) {
         <h3>
           <Link href={`/users/${id}`}>{name}</Link>
         </h3>
-        <p>Age: {age}</p>
+        <p>Age: {age ?? 'unknown'}</p>
       </div>
     </div>
   );
